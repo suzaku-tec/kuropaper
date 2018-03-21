3ca0dfd2ce83f6937be44c4d1830df766912bc09
IkarashiToshio <jhypd380@yahoo.co.jp>
2018-03-21 22:48:31 +0900

    文節の主語・述語の分類付与を実装

--- src/test/java/sentence/ParagraphTest.java
+++ src/test/java/sentence/ParagraphTest.java
@@ -3,12 +3,20 @@ package sentence;
 import exception.ParagraphException;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
 
 public class ParagraphTest {
-    @Test
-    public void testClause() throws ParagraphException {
-        Clause c = new Clause("すぐに出発しなければならない。");
-    }
+
+	@Test
+	public void testPredicate() throws ParagraphException {
+		Clause c = new Clause("誰も僕の言うことを信じない。");
+		List<Paragraph> paragraphList = c.getParagraphList();
+		List<Paragraph> list = paragraphList.stream().filter(paragraph -> Paragraph.WorkType.PREDICATE.equals(paragraph.getWorkType())).collect(Collectors.toList());
+		assertEquals("述語判定エラー", "信じない。", list.get(list.size() - 1).getParagraph());
+	}
 
 }
