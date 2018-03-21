af29fb3ca8ceae578dbb532ce54144afed4b287c
IkarashiToshio <jhypd380@yahoo.co.jp>
2018-03-21 23:29:23 +0900

    文節の主語・述語の分類付与テストを追加 ※まだ通ってない

--- src/test/java/sentence/ClauseTest.java
+++ src/test/java/sentence/ClauseTest.java
@@ -4,6 +4,7 @@ import exception.ParagraphException;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.junit.Assert.*;
@@ -53,8 +54,85 @@ public class ClauseTest {
 				, c.getParagraphList().stream().map(paragraph -> paragraph.getParagraph()).collect(Collectors.toList()));
 	}
 
+	/**
+	 * どっちかというと、kuromojiが動くことのテスト
+	 */
+	@Test
+	public void testWordList1() {
+		List<Word> wordList = Clause.createWordList("山椒魚は悲しんだ");
+		assertEquals("単語：" + wordList.stream().map(word -> word.getSurface()).collect(Collectors.joining(PARAGRAPH_CHAR)), 4, wordList.size());
+	}
+
+	/**
+	 * どっちかというと、kuromojiが動くことのテスト
+	 */
+	@Test
+	public void testWordList2() {
+		List<Word> wordList = Clause.createWordList("菜の花が咲く季節になりました");
+		assertEquals("単語：" + wordList.stream().map(word -> word.getSurface()).collect(Collectors.joining(PARAGRAPH_CHAR)), 8, wordList.size());
+	}
+
+	/**
+	 * どっちかというと、kuromojiが動くことのテスト
+	 */
+	@Test
+	public void testWordList3() {
+		List<Word> wordList = Clause.createWordList("大人らしく落ち着いて話してください");
+		assertEquals("単語：" + wordList.stream().map(word -> word.getSurface()).collect(Collectors.joining(PARAGRAPH_CHAR)), 7, wordList.size());
+	}
+
+	@Test
+	public void testPredicate1() throws Exception {
+		Clause c = new Clause("誰も僕の言うことを信じない。");
+		assertEquals("述語判定エラー", "信じない。", c.getPredicate().getParagraph());
+	}
+
+	@Test
+	public void testSubject1() throws Exception {
+		Clause c = new Clause("誰も僕の言うことを信じない。");
+		assertEquals("主語判定エラー", "誰も", c.getSubject().orElseThrow(() ->new Exception()).getParagraph());
+	}
+
+	@Test
+	public void testPredicate2() throws Exception {
+		Clause c = new Clause("あの映画のファンが続編を待ち望んでいる。");
+		assertEquals("述語判定エラー", "待ち望んでいる。", c.getPredicate().getParagraph());
+	}
+
+	@Test
+	public void testSubject2() throws Exception {
+		Clause c = new Clause("あの映画のファンが続編を待ち望んでいる。");
+		assertEquals("主語判定エラー", "ファンが", c.getSubject().orElseThrow(() ->new Exception()).getParagraph());
+	}
+
+	@Test
+	public void testPredicate3() throws Exception {
+		Clause c = new Clause("ああ、きれいだなあ、あの人は。");
+		assertEquals("述語判定エラー", "きれいだなあ、", c.getPredicate().getParagraph());
+	}
+
+	@Test
+	public void testSubject3() throws Exception {
+		Clause c = new Clause("ああ、きれいだなあ、あの人は。");
+		assertEquals("主語判定エラー", "人は。", c.getSubject().orElseThrow(() ->new Exception()).getParagraph());
+	}
+
+	@Test
+	public void testPredicate4() throws Exception {
+		Clause c = new Clause("山道を登りながら、こう考えた。");
+		assertEquals("述語判定エラー", "考えた。", c.getPredicate().getParagraph());
+	}
+
+	@Test
+	public void testSubject4() throws Exception {
+		Clause c = new Clause("山道を登りながら、こう考えた。");
+		assertEquals("主語判定エラー", null, c.getSubject().get());
+	}
+
 
 	private String getparaParagraphListStr(Clause c) {
 		return c.getParagraphList().stream().map(paragraph -> paragraph.getParagraph()).collect(Collectors.joining(PARAGRAPH_CHAR));
 	}
+
+
 }
